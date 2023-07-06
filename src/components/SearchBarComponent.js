1b64ac4a54e21f17ac94c575c6a3c83b6c607314
Thomas Kirby <tmkirby@umich.edu>
2023-07-06 11:13:27 -0400

    Change

--- src/components/SearchBarComponent.js
+++ src/components/SearchBarComponent.js
@@ -23,13 +23,15 @@ export class SearchBarComponent extends React.Component {
         return (
             <div>
                 <div style={{clear: 'both'}}>{this.state.guesses[index][0] + " " + this.state.guesses[index][1]}</div>
-                <div style={{display: 'table'}}>
+                <table cellSpacing='5px' style={{margin: '0 auto', width: '80%'}}>
                     {this.state.guesses[index].splice(2, 10).map((attr, index1) => {
+                        return (
                         <div class={index1 < 2 ? "first2" : index < 4 ? "second2" : "third2"}>
                             {attr}
                         </div>
+                        )
                     })}
-                </div>
+                </table>
             </div>
         )
     }
@@ -41,7 +43,7 @@ export class SearchBarComponent extends React.Component {
 
     render() {
         return (
-            <div>
+            <div style={{width: '100%'}}>
             <div class="box">
                 <form name="search" onSubmit = {(e) => this.acceptGuess(e)}>
                     <input type="text" style={{caretColor: 'transparent'}}  placeholder="Guess Here" id='search' class="input" name="txt" onKeyUp={(e)=>this.checkAns(e)} />
