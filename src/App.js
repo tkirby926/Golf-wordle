ed9f62806b8965882e75dc5cc08db7f091929723
Thomas Kirby <tmkirby@umich.edu>
2023-10-19 19:25:33 -0400

    Change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomeComponent } from './components/HomeComponent';
-import HomePhoto from './components/golfle.jpeg';
+import HomePhoto from './components/golfle1.jpeg';
 import React, { useState } from 'react';
 
 function App() {
