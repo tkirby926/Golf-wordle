4e7715b2f977071d7e15fd0bc9495ec080a9ce09
Thomas Kirby <tmkirby@umich.edu>
2023-10-23 22:39:26 -0400

    Change

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -15,8 +15,8 @@ export class HomeComponent extends React.Component {
             <html>
             <div style={{width: '100%'}}>
                 <head>
-                {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6567831601079042"
-     crossorigin="anonymous"></script> */}
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6567831601079042"
+     crossorigin="anonymous"></script>
                 </head>
                 <body>
                 <div class="center_text">
