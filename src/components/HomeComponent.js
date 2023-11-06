d29e911785cb928d32fa3a42ec0bd9cade38606c
Thomas Kirby <tmkirby@umich.edu>
2023-11-06 16:13:53 -0500

    Change

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -12,20 +12,12 @@ export class HomeComponent extends React.Component {
 
     render() {
         return (
-            <html>
             <div style={{width: '100%'}}>
-                <head>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6567831601079042"
-     crossorigin="anonymous"></script>
-                </head>
-                <body>
                 <div class="center_text">
                     <SearchBarComponent />
                 </div>
-                </body>
                 {/* <FooterComponent /> */}
             </div>
-            </html>
         )
     }
 
