ae378d65f9ccf73f1d6215d2b99b9b940f1e75b0
Thomas Kirby <tmkirby@umich.edu>
2023-11-09 19:26:08 -0500

    Change

--- src/components/admin.js
+++ src/components/admin.js
@@ -19,22 +19,33 @@ export class AdminComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
+            users_signed: [],
+            users_not: [],
             user_count: 0,
-            page: 0
+            page_signed: 0,
+            page_not: 0
         }
     }
-    changePage(e, next) {
+    changePage(e, next, signed) {
         e.preventDefault();
-        var page = next ? this.state.page + 1 : this.state.page - 1;
-        fetch(UserProfile.getUrl() + '/nextadminpage/' + page, { credentials: 'include', method: 'GET' })
+        var page = next ? this.state.page_not + 1 : this.state.page_not - 1;
+        if (signed) {
+            page = next ? this.state.page_signed + 1 : this.state.page_signed - 1;
+        }
+        var signed_char = signed ? '1' : '0';
+        fetch(UserProfile.getUrl() + '/nextadminpage/' + page + "/" + signed_char, { credentials: 'include', method: 'GET' })
         .then(response => response.json())
         .then((data) => {
             if (data.auth == 'failed') {
                 window.location.assign('/log_adm')
             }
             else {
-                this.setState({users: data.users, page: page})
+                if (signed) {
+                    this.setState({users_signed: data.users, page_signed: page})
+                }
+                else {
+                    this.setState({users_not: data.users, page_not: page})
+                }
             }
         });
     }
@@ -44,15 +55,28 @@ export class AdminComponent extends React.Component {
         <div class="big_form_white">
             <p>User Count: {this.state.user_count}</p><br></br>
             <p>Users:</p>
-            {this.state.users.map((user, index) => {
-                return (
-                    <div>
-                        {user}
-                    </div>
-                )
-            })}
-            <button disabled={this.state.page == 0} onClick={(e) => this.changePage(e, false)}>Last Page</button>
-            <button disabled={this.state.users.length != 30} onClick={(e) => this.changePage(e, true)}>Next Page</button>
+            <div style={{float: 'left'}}>
+                {this.state.users_signed.map((user, index) => {
+                    return (
+                        <div>
+                            {user}
+                        </div>
+                    )
+                })}
+                <button disabled={this.state.page_signed == 0} onClick={(e) => this.changePage(e, false, true)}>Last Page</button>
+                <button disabled={this.state.users_signed.length != 30} onClick={(e) => this.changePage(e, true, true)}>Next Page</button>
+            </div>
+            <div style={{float: 'right'}}>
+            {this.state.users_not.map((user, index) => {
+                    return (
+                        <div>
+                            {user}
+                        </div>
+                    )
+                })}
+                <button disabled={this.state.page_not == 0} onClick={(e) => this.changePage(e, false, false)}>Last Page</button>
+                <button disabled={this.state.users_not.length != 30} onClick={(e) => this.changePage(e, true, false)}>Next Page</button>
+            </div>
         </div>
         )
     }
